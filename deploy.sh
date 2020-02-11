1b5235fbe22c04ce33f8864aa7f900213d1b0973
ummerh <harsha.madathil@gmail.com>
2020-02-11 18:34:49 +0000

    modular deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,26 @@
 
 #!/bin/bash
 
-az acr login --resource-group=dc50-eacloud-temp-rg002 --name=eacloudacr
+login_docker(){
+    az acr login --resource-group=dc50-eacloud-temp-rg002 --name=eacloudacr
+}
+login_aks(){
+    az aks get-credentials --resource-group=dc50-eacloud-temp-rg002 --name=eacloudAKS
+}
 
-az aks get-credentials --resource-group=dc50-eacloud-temp-rg002 --name=eacloudAKS
+build_app(){
+   git pull
+   mvn clean package
+}
 
-git pull
+build_docker(){
+   docker build . -t eacloudacr.azurecr.io/eacloud/eforms
+   docker tag eacloudacr.azurecr.io/eacloud/eforms eacloudacr.azurecr.io/eacloud/eforms:latest
+   docker push  eacloudacr.azurecr.io/eacloud/eforms
+}
 
-mvn clean package
-
-docker build . -t eacloudacr.azurecr.io/eacloud/eforms
-
-docker tag eacloudacr.azurecr.io/eacloud/eforms eacloudacr.azurecr.io/eacloud/eforms:latest
-
-docker push  eacloudacr.azurecr.io/eacloud/eforms
-
+login_docker
+login_aks
+build_app
+build_docker
 kubectl apply -f eforms-aks.yml
